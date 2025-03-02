4653028759e463a3d385ac70809451c2b13a489b
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2025-03-02 16:33:27 +0530

    added canvas controls

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -8,6 +8,10 @@ import {
 } from "react-native";
 import { Canvas, Path } from "@shopify/react-native-skia";
 import React, { useState } from "react";
+import { Colors } from "@/constants/Colors";
+
+const colors = ["black", "red", "blue", "green", "purple"];
+const strokeWidths = [2, 4, 6, 8, 10];
 
 interface Props {
   photo: string | null;
@@ -19,8 +23,14 @@ const CanvasContainer: React.FC<Props> = ({ photo, setPhoto }) => {
   const [currentPath, setCurrentPath] = useState<string>("");
   const [strokeColor, setStrokeColor] = useState<string>("black");
   const [strokeWidth, setStrokeWidth] = useState<number>(4);
+  const [isDrawingEnabled, setIsDrawingEnabled] = useState<boolean>(false);
+  const [isColorPickerEnabled, setIsColorPickerEnabled] =
+    useState<boolean>(false);
+  const [isStrokePickerEnabled, setIsStrokePickerEnabled] =
+    useState<boolean>(false);
 
   const handleTouchStart = (event: GestureResponderEvent): void => {
+    if (!isDrawingEnabled) return;
     const touch = event.nativeEvent.touches[0]; // Get the first touch point
     if (touch) {
       setCurrentPath(`M${touch.locationX},${touch.locationY}`);
@@ -28,6 +38,7 @@ const CanvasContainer: React.FC<Props> = ({ photo, setPhoto }) => {
   };
 
   const handleTouchMove = (event: GestureResponderEvent): void => {
+    if (!isDrawingEnabled) return;
     const touch = event.nativeEvent.touches[0];
     if (touch) {
       setCurrentPath(
@@ -37,6 +48,7 @@ const CanvasContainer: React.FC<Props> = ({ photo, setPhoto }) => {
   };
 
   const handleTouchEnd = (): void => {
+    if (!isDrawingEnabled) return;
     setPaths([...paths, currentPath]);
     setCurrentPath("");
   };
@@ -76,20 +88,99 @@ const CanvasContainer: React.FC<Props> = ({ photo, setPhoto }) => {
           />
         )}
       </Canvas>
-      <View style={styles.controls}>
-        <TouchableOpacity onPress={clearCanvas}>
-          <View style={styles.btn}>
-            <Text style={styles.btnText}>Clear</Text>
-          </View>
-        </TouchableOpacity>
+
+      <View style={styles.controlsTop}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "70%",
+            marginHorizontal: "auto",
+            borderRadius: 32,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              setIsColorPickerEnabled(false);
+              setIsStrokePickerEnabled(false);
+              setIsDrawingEnabled(!isDrawingEnabled);
+            }}
+          >
+            <Text style={[styles.btnText, styles.controlBtn]}>
+              {isDrawingEnabled ? "Disable Draw" : "Enable Draw"}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              setIsStrokePickerEnabled(false);
+              setIsColorPickerEnabled(!isColorPickerEnabled);
+            }}
+            disabled={!isDrawingEnabled}
+          >
+            <Text style={[styles.btnText, styles.controlBtn]}>Color</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              setIsColorPickerEnabled(false);
+              setIsStrokePickerEnabled(!isStrokePickerEnabled);
+            }}
+            disabled={!isDrawingEnabled}
+          >
+            <Text style={[styles.btnText, styles.controlBtn]}>Stroke</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.controlsNew}>
-        <TouchableOpacity onPress={discardPhoto}>
-          <View style={styles.btn}>
-            <Text style={styles.btnText}>discard</Text>
-          </View>
-        </TouchableOpacity>
+
+      <View style={styles.controlsBottom}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <TouchableOpacity onPress={discardPhoto}>
+            <Text style={[styles.btnText, styles.controlBtn]}>back</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={clearCanvas}>
+            <Text style={[styles.btnText, styles.controlBtn]}>
+              Clear Canvas
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
+
+      {isColorPickerEnabled && (
+        <View style={styles.controls}>
+          <View style={styles.selectorContainer}>
+            {colors.map((color) => (
+              <TouchableOpacity
+                key={color}
+                onPress={() => setStrokeColor(color)}
+                style={[styles.colorBox, { backgroundColor: color }]}
+              />
+            ))}
+          </View>
+        </View>
+      )}
+
+      {isStrokePickerEnabled && (
+        <View style={styles.controls}>
+          <View style={styles.selectorContainer}>
+            {strokeWidths.map((width) => (
+              <TouchableOpacity
+                key={width}
+                onPress={() => setStrokeWidth(width)}
+                style={styles.strokeBox}
+              >
+                <Text>{width}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+      )}
     </View>
   );
 };
@@ -118,23 +209,61 @@ const styles = StyleSheet.create({
   },
   btnText: {
     textAlign: "center",
-    fontSize: 25,
+    fontSize: 16,
     marginRight: 10,
   },
   controls: {
     position: "absolute",
-    bottom: 20,
-    right: 50,
+    top: 60,
     width: "100%",
-    flexDirection: "row",
-    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    paddingHorizontal: 10,
   },
-  controlsNew: {
+  controlsTop: {
+    position: "absolute",
+    top: 20,
+    width: "90%",
+    backgroundColor: Colors.PRIMARY,
+    padding: 5,
+    left: "50%",
+    transform: [{ translateX: "-50%" }],
+    borderRadius: 32,
+  },
+  controlsBottom: {
     position: "absolute",
     bottom: 20,
-    right: -50,
-    width: "100%",
+    width: "90%",
+    left: "50%",
+    transform: [{ translateX: "-50%" }],
+    borderRadius: 32,
+  },
+  selectorContainer: {
+    display: "flex",
     flexDirection: "row",
     justifyContent: "center",
+    gap: 10,
+    marginTop: 10,
+    backgroundColor: "#ffffff",
+    width: "70%",
+    marginHorizontal: "auto",
+    borderRadius: 10,
+  },
+  colorBox: { width: 25, height: 25, margin: 5, borderRadius: 5 },
+  strokeBox: {
+    width: 25,
+    height: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    margin: 5,
+    borderRadius: 5,
+  },
+  controlBtn: {
+    backgroundColor: Colors.LIGHT_GREEN,
+    paddingHorizontal: 10,
+    borderRadius: 30,
+    fontWeight: "bold",
+    color: Colors["theme-gray"][800],
   },
 });
